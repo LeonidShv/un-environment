a6657ba2fbef16b8371cede1b5d5b7949134118c
LeonidShv <matembit1996@gmail.com>
2024-01-15 11:25:34 +0200

    feat: setup datepicker component

---
+++ components/VDatePicker.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-date-picker
+    @change="(value) => $emit('update:v-model', value)"
+    v-model="value1"
+    :type="type"
+    :placeholder="placeholder"
+    size="default"
+    value-format="YYYY-MM-DD"
+  />
+</template>
+
+<script setup>
+defineProps({
+  loading: {
+    type: Boolean,
+    default: false
+  },
+  placeholder: {
+    type: String,
+    default: ''
+  },
+  type: {
+    type: String,
+    default: 'date',
+    validator(value) {
+      return ['year', 'date', 'dates', 'daterange'].includes(value)
+    }
+  },
+  size: {
+    type: String,
+    default: 'default',
+    validator(value) {
+      return ['default', 'large', 'small'].includes(value)
+    }
+  }
+})
+</script>
