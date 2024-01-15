1d0b9a26aeb3fd4695217d1dafa1c3df417f5787
LeonidShv <matembit1996@gmail.com>
2024-01-15 11:22:15 +0200

    infra: dynamic params in readEnvironment

--- api/environment.js
+++ api/environment.js
@@ -1,5 +1,5 @@
 import { api } from "@/lib/axios";
 
 export default {
-  readEnvironment: (element, country) => api.get(`/.${element}.${country}./ALL/?detail=full&startPeriod=1960-01-01&endPeriod=2020-12-31&dimensionAtObservation=TIME_PERIOD`)
+  readEnvironment: (element, country, params) => api.get(`/.${element}.${country}./ALL/`, { params })
 };
