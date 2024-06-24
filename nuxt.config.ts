0f0fb309312e6d844e9d74ea3807b7bb253a1d1c
LeonidShv <matembit1996@gmail.com>
2024-06-24 12:46:48 +0300

    infra: github pages deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,10 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@element-plus/nuxt", "@pinia/nuxt"],
-  // eslint: {
-  // },
+  app: {
+    baseURL: "/un-environment/", // baseURL: '/<repository>/'
+    buildAssetsDir: "assets", // don't use "_" at the begining of the folder name to avoids nojkill conflict
+  },
   vite: {
     css: {
       preprocessorOptions: {
