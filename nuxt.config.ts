fc5aca64f3b86477f0878c11ee99a1e8b529ca92
LeonidShv <matembit1996@gmail.com>
2024-06-24 22:24:46 +0300

    infra: fix cd deplouy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: true },
   modules: ["@element-plus/nuxt", "@pinia/nuxt", "@nuxt/image-edge"],
   app: {
